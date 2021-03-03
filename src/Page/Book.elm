fd923e42d165397ad8ebb41a6d083d802ba09a88
中村碧衣 <1186981@st.anan-nct.ac.jp>
2021-03-03 14:50:06 +0900

    単純な本の情報を表示

--- src/Page/Book.elm
+++ src/Page/Book.elm
@@ -1,16 +1,24 @@
 module Page.Book exposing (..)
 
+import Data.Book exposing (Book)
 import Html exposing (..)
+import Html.Attributes exposing (src)
 import Session exposing (Session)
 
 
 type alias Model =
-    { session : Session }
+    { session : Session
+    , book : Book
+    }
 
 
 init : Session -> String -> ( Model, Cmd Msg )
 init session _ =
-    ( { session = session }, Cmd.none )
+    ( { session = session
+      , book = Book "book-0000" "桃太郎" "昔話" "不明" "user-0000"
+      }
+    , Cmd.none
+    )
 
 
 type Msg
@@ -28,5 +36,17 @@ update _ model =
 
 
 view : Model -> { title : String, body : List (Html Msg) }
-view _ =
-    { title = "本", body = [ text "本 - 作成中" ] }
+view model =
+    { title = "本"
+    , body =
+        [ div [] [ text model.book.title ]
+        , div []
+            [ img [ src "https://3.bp.blogspot.com/-ZaEM1sNjZ10/VtofN5FQWMI/AAAAAAAA4WI/W8nM9jXMwnA/s200/book_sasshi1_red.png" ] []
+            , div []
+                [ div [] [ text model.book.category ]
+                , div [] [ text model.book.author ]
+                , div [] [ text model.book.user ]
+                ]
+            ]
+        ]
+    }
